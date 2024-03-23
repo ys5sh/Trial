cbeda3a258d3a9e1baaf744123dc3fed0b033797
oSo <ys5shso@gmail.com>
2024-03-24 00:07:43 +0530

    intial version

---
+++ PWA/Question/Opps1.java
@@ -0,0 +1,16 @@
+public class Opps1 {
+    String name;
+    int number;
+
+    public Opps1(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public static void main(String[] args) {
+        Opps1 s1 = new Opps1("tushar", 23);
+        System.out.println(s1.name +" "+ s1.number);
+    }
+}
+
+
